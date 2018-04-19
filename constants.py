6476d8eae33e0a0ec0f8a6eaf84a837f77eefce2
popeg <przemek.mazgaj@gmail.com>
2018-04-19 23:37:03 +0200

    Fix importing csv

--- constants.py
+++ constants.py
@@ -6,4 +6,4 @@ __author__ = "Przemek"
 import os
 
 PROJECT_PATH = os.path.split(os.path.abspath(__file__))[0]
-CSV_FILE_PATH = os.path.join(PROJECT_PATH, 'csv')
+CSV_FILE_PATH = os.path.join(PROJECT_PATH, 'tresc_zadania', 'csv')
