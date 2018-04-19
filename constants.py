86b3c8803c4c23aceefdbad8858a88c6a8c4f6d5
popeg <przemek.mazgaj@gmail.com>
2018-04-19 23:33:40 +0200

    Cleanup

---
+++ constants.py
@@ -0,0 +1,9 @@
+"""
+"""
+
+__author__ = "Przemek"
+
+import os
+
+PROJECT_PATH = os.path.split(os.path.abspath(__file__))[0]
+CSV_FILE_PATH = os.path.join(PROJECT_PATH, 'csv')
--- utils/parse_csv.py
+++ utils/parse_csv.py
@@ -2,40 +2,47 @@
 Parse given csv files, to proper format.
 """
 import csv
+import os
+
+from constants import CSV_FILE_PATH
 
 __author__ = "Przemek"
 
 
-def parse_shop():
-    path = "C:/IMS_ZADANIE/Zadanie/Pliki/shops.csv"
-    """
-    parse information from shop file
-    """
-    reader = csv.DictReader(open(path))
-    shops_list = []
-    for row in reader:
-        shops_list.append(row)
-    return shops_list
+class CsvParsers:
+    @staticmethod
+    def parse_shop():
+        """
+        parse information from shop file
+        """
+        path = os.path.join(CSV_FILE_PATH, 'categories.csv')
+        reader = csv.DictReader(open(path))
+        shops_list = []
+        for row in reader:
+            shops_list.append(row)
+        return shops_list
+
+    @staticmethod
+    def parse_category():
+        """
+        parse information for category
+        """
+        path = os.path.join(CSV_FILE_PATH, 'categories.csv')
+        id_list = []
+        visible_list = []
+        name_list = []
+        categories = []
+        reader = csv.DictReader(open(path))
+        for row in reader:
+            id_list.append(row.get('id', 0))
+            visible_list.append(row.get('visible', False))
+            name_list.append(row.get('name', '--name--'))
+            categories.append(row)
+
+        return categories
 
 
-def parse_category():
-    path = "C:/IMS_ZADANIE/Zadanie/Pliki/categories.csv"
-    """
-    parse information for category
-    """
-    id_list = []
-    visible_list = []
-    name_list = []
-    categories = []
-    reader = csv.DictReader(open(path))
-    for row in reader:
-        id_list.append(row.get('id', 0))
-        visible_list.append(row.get('visible', False))
-        name_list.append(row.get('name', '--name--'))
-        categories.append(row)
-
-    return categories
-
-
-# parse_category()
-parse_shop()
+if __name__ == '__main__':
+    csv_parser = CsvParsers()
+    # parse_category()
+    csv_parser.parse_shop()
